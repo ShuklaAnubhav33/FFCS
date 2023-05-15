41b60170f0dcd172cf2c271dd8bfcc430eb7bb7f
Anubhav Shukla <99384578+ShuklaAnubhav33@users.noreply.github.com>
2023-05-15 10:48:58 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; <?php echo date('Y');?> VIT-FFCS | By : <a href=" https://anubhavshukla9-vit-portfolio.netlify.app" target="_blank">ANUBHAV SHUKLA</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
